e53ea64c084f4de5719f03a17e9e9e835d9ebad7
aziza20-ux <s.afadhali@alustudent.com>
2025-06-04 15:58:25 +0200

    Revert "the updated console"
    This reverts commit 5dfe6251d01d2aa18caa5df22ff853d4b0795a38.
    
    the deleted files

--- README.md
+++
@@ -1,2 +0,0 @@
-this project is about making bnb platform for paople to sort through avaible houses and amenities 
-
--- tests/test_models/main.py
+++
@@ -1,4 +0,0 @@
-from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-
-# Example usage or test code
