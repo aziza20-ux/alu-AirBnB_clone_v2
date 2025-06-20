9d7aff92b8f7b2d571917210b5eea70839350565
aziza20-ux <s.afadhali@alustudent.com>
2025-06-20 12:05:11 +0200

    the

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,11 @@
---thr econnection
---database
+-- MySQL server for the project
+-- GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
 USE hbnb_dev_db;
-GRANT ALL PRIVILEDGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 USE performance_schema;
-GRANT SELECT ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEDGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
 SET FOREIGN_KEY_CHECKS=1;
