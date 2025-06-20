dd5a8047fb7d7f1932aaa1bca9c20c647128ad42
aziza20-ux <s.afadhali@alustudent.com>
2025-06-20 12:01:49 +0200

    the commit

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,5 @@
-#thr econnection
+--thr econnection
+--database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 USE hbnb_dev_db;
