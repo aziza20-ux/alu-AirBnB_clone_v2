4b402b9f3af7608953ea4ba353ca8f95419c3e82
aziza solace afadhali <s.afadhali@alustudent.com>
2025-06-20 12:28:58 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,69 +1,66 @@
 #!/usr/bin/python3
-
-import uuid
-from datetime import datetime
+"""Defines the BaseModel class for base."""
 import models
+from uuid import uuid4
+from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import column
-from sqlalchemy import string
-from sqlalchemy import table
-from sqlalchemy import float
-
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import String
 
 Base = declarative_base()
 
 
 class BaseModel:
-    id = column(string(60), nullable=False, primary_key=True)
-    created_at = column(datetime, nullable=False, default=datetime.now())
-    updated_at = column(datetime, nullable=False, default=datetime.now())
+    """Defines the BaseModel class.
+    Attributes:
+        id (sqlalchemy String): The BaseModel id.
+        created_at (sqlalchemy DateTime): The datetime at creation.
+        updated_at (sqlalchemy DateTime): The datetime of last update.
+    """
 
-    """the constructor of the class '__init__'"""
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
-    def __init__(self, *args, **kwargvs):
-
-        if kwargvs:
-            for k, v in kwargvs.items():
-                if k == '__class__':
-                    continue
-
-                elif k in ["created_at", "updated_at"]:
-                    setattr(self, k, datetime.fromisoformat(v))
-                else:
-                    setattr(self, k, v)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-    def __str__(self):
-        """it will return the string represation of object"""
-
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def save(self):
-        """this method will save the last time object was modified"""
-
-        self.updated_at = datetime.now()
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        """the method to returm dictionary of the attributes of any object"""
-        obj_dict = self.__dict__.copy()
-
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        obj_dict.pop("_sa_instance_state", None)
-
-        return obj_dict
+        """Return dict rep of the BaseModel instance.
+    
+        """
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict.pop("_sa_instance_state", None)
+        return my_dict
 
     def delete(self):
+        """Delete the current instance from storage."""
         models.storage.delete(self)
 
-
-if __name__ == '__main__':
-
-    c = BaseModel(city="aziza", id="2")
-    c.save()
+    def __str__(self):
+        """Return str representation of the BaseModel instance."""
+        d = self.__dict__.copy()
+        d.pop("_sa_instance_state", None)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, d)
